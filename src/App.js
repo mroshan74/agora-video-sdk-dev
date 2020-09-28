7e6dbc4475b945abe9244daf183249bf5deebbbb
Tacnik <roshan@tacnik.com>
2020-09-28 19:35:05 +0530

    faulty fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import AgoraRTC from 'agora-rtc-sdk'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const appID = '4b88997c353f432fbad18f5305e426a8'
+
+const App = () => {
+  const [rtc, setRtc] = useState({
+    client: null,
+    joined: false,
+    published: false,
+    localStream: null,
+    remoteStreams: [],
+    params: {}
+  })
+
+  const [option, setOption] = useState({
+    appID,
+    channel: "demoChannel", // dynamic unique
+    uid: "random",  // dynamic unique
+    token: "Your token" // optional --> create in the server
+  })
+  console.log(option.appID)
+  useEffect(() => {
+    // rtc.localStream = AgoraRTC.createStream({
+    //   streamID: option.uid,
+    //   audio: true,
+    //   video: true,
+    //   screen: false,
+    // });
+
+    // // Initialize the local stream
+    // rtc.localStream.init(function () {
+    //   console.log("init local stream success");
+    //   // play stream with html element id "local_stream"
+    //   rtc.localStream.play("local_stream");
+    // }, function (err) {
+    //   console.error("init local stream failed ", err);
+    // });
+
+    // // Publish the local stream
+    setRtc({
+      ...rtc,
+      client: AgoraRTC.createClient({mode: "rtc", codec: "h264"}),
+      localStream :  AgoraRTC.createStream({
+        streamID: option.uid,
+        audio: true,
+        video: true,
+        screen: false,
+      })
+    })
+  },[])
+
+  
+
+  if(rtc.localStream){
+     // Initialize the local stream
+    rtc.localStream.init(function () {
+      console.log("init local stream success");
+      // play stream with html element id "local_stream"
+      rtc.localStream.play("local_stream");
+    }, function (err) {
+      console.error("init local stream failed ", err);
+    });
+
+    // Publish the local stream
+    console.log('[rtc.client]', rtc.client)
+    rtc.client.publish(rtc.localStream, function (err) {
+      console.log("publish failed");
+      console.error(err);
+    })
+  }
+  
+  return(
+    <div>
+      <h2>Code goes here...</h2>
+      <div id='local_stream'></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import App from './App'
+
+const Routes = () => (
+    <Fragment>
+        <h1>videoCall - alphaBuild v0.0</h1>
+        <App />
+    </Fragment>
+)
+
+export default Routes
